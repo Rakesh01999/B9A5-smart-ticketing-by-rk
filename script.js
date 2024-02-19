99d7a6b1ad558b61a7a91897d34ccb43f91afa79
Rakesh01999 <rbiswas01999@gmail.com>
2024-02-19 23:33:18 +0600

    added

--- script.js
+++ script.js
@@ -20,18 +20,24 @@ for (let i = 0; i < seats.length; i++) {
         const seatName = seat.querySelector('span').innerText;
         // console.log(seatName) ;
 
+        seatCounter++;
+        totalSeat--;
+
+        totalPrice += 550;
         // set bg color
+        if(seatCounter>4){
+            alert('please select less than or equal to FOUR seat') ;
+            return false ;
+        }
         event.target.style.backgroundColor = '#1DD100';
 
         const seatNameContainer = document.getElementById('seatName-container');
 
         const p = document.createElement('p');
-        p.innerText = seatName + " " + 'Economoy' + ' ' + '550';
+        p.innerText = seatName + " " + 'Economoy' + " " + '550';
         seatNameContainer.appendChild(p);
-        seatCounter++;
-        totalSeat--;
-
-        totalPrice += 550;
+        
+        
         // console.log(totalPrice) ;
         document.getElementById('seat-counter').innerText = seatCounter;
         document.getElementById('total-seat').innerText = totalSeat;
@@ -39,9 +45,48 @@ for (let i = 0; i < seats.length; i++) {
         document.getElementById('grand-total').innerText = 'BDT' + ' ' + totalPrice;
         // const grandTotal = document.getElementById('grand-total') ;
 
+        // check validation of stcntr & phnnmbr
+
+        // Get the input element by its ID
+        let phoneNumberInput = document.getElementById('number');
+        
+        // Check if the input field is non-empty when a form is submitted
+
+        // Check if the value of the input field is empty
+        if (seatCounter>=1 && phoneNumberInput.value.trim() != '') {
+            // If empty, show an alert message
+            // alert('Phone number field is required!');
+            // Prevent the form from submitting
+            // Enable the button
+            button.disabled = false;
+        }
+
     });
 }
 
+// let phoneNumberInput = document.getElementById('number');
+//     if (seatCounter>=1 && seatCounter<=4 && phoneNumberInput.value.trim() != '') {
+//         // Enable the button
+//         button.disabled = false;
+//     }
+//     else {
+//         alert('Please select minimum 1 and maximum 4 seat');
+//         // document.getElementById('input-field').value = '';
+//          // Disble the button
+//          button.disabled = true;
+//     }
+
+var phoneNumberInput = document.getElementById('number');
+
+    phoneNumberInput.addEventListener('blur', function() {
+        if (phoneNumberInput.value.trim() === '') {
+            alert('Phone number field is required!');
+        }
+        else{
+            // Enable the button
+            button.disabled = false;
+        }
+    });
 
 const applyBtn = document.getElementById('apply-btn');
 
@@ -69,8 +114,10 @@ applyBtn.addEventListener('click', function () {
             const total = document.getElementById('grand-total');
             total.innerText = 'BDT' + ' ' + restTotal.toFixed(2);
             document.getElementById('input-field').value = '';
-            // Enable the button
-            button.disabled = false;
+            if (seatCounter>=1 && phoneNumberInput.value.trim() != '') {
+                // Enable the button
+                button.disabled = false;
+            }
         }
         else if (couponCode == 'Couple 20') {
             // console.log( totalPrice) ;
@@ -83,8 +130,10 @@ applyBtn.addEventListener('click', function () {
             const total = document.getElementById('grand-total');
             total.innerText = 'BDT' + ' ' + restTotal.toFixed(2);
             document.getElementById('input-field').value = '';
-            // Enable the button
-            button.disabled = false;
+            if (seatCounter>=1 && phoneNumberInput.value.trim() != '') {
+                // Enable the button
+                button.disabled = false;
+            }
         }
         else {
             alert('Invalid Coupon Code');
@@ -97,7 +146,20 @@ applyBtn.addEventListener('click', function () {
     }
 });
 
+// next-button
+// const nextBtn = document.getElementById('btn');
 
+//     // let phoneNumberInput = document.getElementById('number');
+//     if (seatCounter>=1 && seatCounter<=4 && phoneNumberInput.value.trim() != '') {
+//         // Enable the button
+//         button.disabled = false;
+//     }
+//     else {
+//         alert('Please select minimum 1 and maximum 4 seat');
+//         // document.getElementById('input-field').value = '';
+//          // Disble the button
+//          button.disabled = true;
+//     }
 
 
 
